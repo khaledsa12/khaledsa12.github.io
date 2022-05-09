1de60afa7bce90462adbdf26315b2ddb72025b01
khaledsa12 <105184243+khaledsa12@users.noreply.github.com>
2022-05-09 13:29:23 +0200

    Update project.js

--- js/project.js
+++ js/project.js
@@ -1,31 +1,3 @@
-let searchBtn = document.getElementById('search_btn');
-let searchForm = document.querySelector('.search');
-
-searchBtn.addEventListener("click",()=>{
-  searchForm.classList.toggle('active');
-});
-
-
-
-  let cardToggle = document.querySelector('#btn_toggle');
-  let header = document.querySelector('.header');
-  let body = document.querySelector('body');
-
-  
-cardToggle.onclick = ()=>{
-  header.classList.toggle('is_dark');
-  body.classList.toggle('is_dark');
-  
-};
-
-
-let navbare = document.querySelector('header').querySelector('nav').querySelectorAll('.nav_head');
-
-navbare.forEach(element =>{
-    element.addEventListener("click",function(){
-        navbare.forEach(re=>re.classList.remove("active"))
-        this.classList.add("active");
-    })});
 
 
 
